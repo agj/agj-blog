05c574b15581341e4fed170a1b49321fae34e188
agj <ale@agj.cl>
2022-10-28 00:27:52 -0300

    Start introducing posts dynamic routes

---
+++ src/Page/Year_/Month_/Post_.elm
@@ -0,0 +1,96 @@
+module Page.Year_.Month_.Post_ exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import DataSource.File
+import DataSource.Glob as Glob exposing (Glob)
+import Head
+import Head.Seo as Seo
+import Html
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    { year : String, month : String, post : String }
+
+
+page : Page RouteParams Data
+page =
+    Page.prerender
+        { head = head
+        , routes = routes
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+routes : DataSource (List RouteParams)
+routes =
+    Glob.succeed RouteParams
+        |> Glob.match (Glob.literal "data/posts/")
+        |> Glob.capture Glob.wildcard
+        |> Glob.match (Glob.literal "/")
+        |> Glob.capture Glob.wildcard
+        |> Glob.match (Glob.literal "/")
+        |> Glob.capture Glob.wildcard
+        |> Glob.match (Glob.literal ".md")
+        |> Glob.toDataSource
+
+
+data : RouteParams -> DataSource Data
+data routeParams =
+    DataSource.succeed ()
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+type alias Data =
+    ()
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    { title = "title"
+    , body =
+        [ Html.text
+            (static.routeParams.year
+                ++ " / "
+                ++ static.routeParams.month
+                ++ " / "
+                ++ static.routeParams.post
+            )
+        ]
+    }
