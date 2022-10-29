c91fd502a3d98fb84bf006494f71f509a89ed149
agj <ale@agj.cl>
2022-10-29 00:24:12 -0300

    Select posts from XML

--- importer/src/importer/core.clj
+++ importer/src/importer/core.clj
@@ -13,10 +13,20 @@
   [& args]
   (println "Hello, World!"))
 
+(def wordpress-xml (-> "wordpress-data.xml"
+                       io/resource
+                       io/file
+                       xml/parse))
+(def posts (->> wordpress-xml
+                first
+                :content
+                first
+                :content
+                (filter (fn [el]
+                          (and (= (:tag el)
+                                  :item)
+                               (= (:tag (first (:content el)))
+                                  :title))))))
+
 (comment
-  (z/xml-zip
-   (-> "wordpress-data.xml"
-      io/resource
-      io/file
-      xml/parse))
-  )
+)
