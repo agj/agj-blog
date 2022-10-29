02c4d9a2f48821c721e181e2efdcb5724c5cfbbe
agj <ale@agj.cl>
2022-10-29 13:00:11 -0300

    Parse post XML data

--- importer/src/importer/core.clj
+++ importer/src/importer/core.clj
@@ -1,32 +1,79 @@
 (ns importer.core
-  (:gen-class) 
+  (:gen-class)
   (:require [clojure.xml :as xml]
             [clojure.java.io :as io]
             [clojure.zip :as z]))
-
-(require '[clojure.zip :as z]
-         '[clojure.java.io :as io]
-         '[clojure.xml :as xml])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+
+;; Helpers
+
+(defn get-tag-text [tag item-xml]
+  (->> item-xml
+       :content
+       (filter (fn [el] (= (:tag el)
+                           tag)))
+       first
+       :content
+       first))
+
+(defn get-taxonomy [domain item-xml]
+  (->> item-xml
+       :content
+       (filter (fn [el] (and (= (:tag el)
+                                :category)
+                             (= (->> el :attrs :domain)
+                                domain))))
+       (map (fn [el]
+              (def attrs (:attrs el))
+              {:name (->> el :content first)
+               :slug (:nicename attrs)}))))
+
+(defn post-to-frontmatter [post-xml]
+  {:title (get-tag-text :title post-xml)
+   :id (get-tag-text :wp:post_id post-xml)
+   :slug (get-tag-text :wp:post_name post-xml)
+   :url (get-tag-text :link post-xml)
+   :date (get-tag-text :wp:post_date post-xml)
+   :categories (get-taxonomy "category" post-xml)
+   :tags (get-taxonomy "post_tag" post-xml)
+   :parent (get-tag-text :wp:post_parent post-xml)
+   :post-type (get-tag-text :wp:post_type post-xml)
+   :status (get-tag-text :wp:status post-xml)
+   :content (get-tag-text :content:encoded post-xml)
+   :description (get-tag-text :description post-xml)
+   :excerpt (get-tag-text :excerpt:encoded post-xml)})
+
+;; Data
+
 (def wordpress-xml (-> "wordpress-data.xml"
                        io/resource
                        io/file
                        xml/parse))
-(def posts (->> wordpress-xml
-                first
-                :content
-                first
-                :content
-                (filter (fn [el]
-                          (and (= (:tag el)
-                                  :item)
-                               (= (:tag (first (:content el)))
-                                  :title))))))
+
+(def items-xml (->> wordpress-xml
+                    :content
+                    first
+                    :content
+                    (filter (fn [el]
+                              (and (= (:tag el)
+                                      :item)
+                                   (= (:tag (first (:content el)))
+                                      :title))))))
+
+(def posts-xml (->> items-xml
+                    (filter (fn [item-xml]
+                              (= (get-tag-text :wp:post_type item-xml)
+                                 "post")))))
+
+
 
 (comment
-)
+  (->> (last posts-xml)
+       post-to-frontmatter)
+  ;;
+  )
