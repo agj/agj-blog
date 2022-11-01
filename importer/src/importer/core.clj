03dc1193636c04cf7e82360a1bd6a62a32b697af
agj <ale@agj.cl>
2022-11-01 13:30:12 -0300

    Start to weave together a URL normalizer for the importer

--- importer/src/importer/core.clj
+++ importer/src/importer/core.clj
@@ -383,5 +383,28 @@
    ;;
    )
 
+  (println
+   (->> ["http://blog.agj.cl/2009/01/campodecolor-got-me-out-of-college/#more-118"
+         "http://www.agj.cl/files/games/campodecolor_memoria.pdf"
+         "http://blog.agj.cl/wp-content/uploads/2009/04/heartlogo1.png"
+         "http://piclog.agj.cl/?picture=89"]
+        (map (fn [url]
+               (let [blog-match (re-matches #".*://blog[.]agj[.]cl(.*)" url)
+                     agj-cl-match (re-matches #".*:(//.*[.]agj[.]cl.*)" url)
+                     wp-content-match (re-matches #".*://blog[.]agj[.]cl/wp-content/uploads/(\d+)/(\d+)/(.*)" url)]
+                 (or (if wp-content-match
+                       (str "/blog.agj.cl/files/"
+                            (get wp-content-match 1) "/"
+                            (get wp-content-match 2) "-"
+                            (get wp-content-match 3))
+                       nil)
+                     (let [blog-url (get blog-match 1)]
+                       (if blog-url
+                         (str/replace blog-url #"#more-\d+" "#language")
+                         nil))
+                     (get agj-cl-match 1)
+                     url))))
+        (str/join "\n")))
+
   ;;
   )
