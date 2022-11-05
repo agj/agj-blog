7f61061447d9d069fcec87500aa7b3d0d4d114bd
agj <ale@agj.cl>
2022-11-04 23:53:19 -0300

    Redirect static tag pages to dynamic route

---
+++ src/Page/Tag/Tag_.elm
@@ -0,0 +1,91 @@
+module Page.Tag.Tag_ exposing (..)
+
+import Data.Tag as Tag
+import DataSource exposing (DataSource)
+import Head
+import Html exposing (Html)
+import Page exposing (Page, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Shared
+import View exposing (View)
+
+
+page : Page RouteParams Data
+page =
+    Page.prerender
+        { head = head
+        , routes = routes
+        , data = data
+        }
+        |> Page.buildNoState
+            { view = view
+            }
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+
+-- ROUTES
+
+
+type alias RouteParams =
+    { tag : String }
+
+
+routes : DataSource (List RouteParams)
+routes =
+    Tag.dataSource
+        |> DataSource.map (List.map (\{ slug } -> { tag = slug }))
+
+
+
+-- DATA
+
+
+type alias Data =
+    ()
+
+
+data : RouteParams -> DataSource Data
+data _ =
+    DataSource.succeed ()
+
+
+
+-- VIEW
+
+
+title : StaticPayload Data RouteParams -> String
+title static =
+    ""
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    [ Head.metaRedirect
+        (Head.raw
+            ("0; url="
+                ++ Tag.toUrl (Tag.get static.sharedData.tags static.routeParams.tag) []
+            )
+        )
+    ]
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    { title = title static
+    , body =
+        []
+    }
