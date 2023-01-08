05b2392c7c5e86cd65ef645ca6ff88043a8872ab
agj <ale@agj.cl>
2023-01-08 12:49:56 -0300

    Switch to using Nix and direnv for dependencies
    (cherry picked from commit 6f3246d57f7ebdba802359b151d0516dc595fd44)

---
+++ default.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.elmPackages.elm
+    pkgs.elmPackages.elm-format
+    pkgs.elmPackages.elm-optimize-level-2
+    pkgs.elmPackages.elm-pages
+    pkgs.elmPackages.elm-review
+    pkgs.nodejs
+  ];
+}
