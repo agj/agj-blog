ab340de2bbb8d4071714b394355a4528bcefb48b
agj <ale@agj.cl>
2023-01-21 10:47:12 -0300

    Make Figure view

---
+++ src/View/Figure.elm
@@ -0,0 +1,40 @@
+module View.Figure exposing
+    ( Figure
+    , figure
+    , view
+    )
+
+import Custom.Color as Color
+import Element as Ui
+import Element.Background as UiBackground
+import Element.Border as UiBorder
+import Style
+
+
+type Figure msg
+    = Figure
+        { content : Ui.Element msg
+        , caption : Maybe String
+        }
+
+
+figure : Ui.Element msg -> Figure msg
+figure content =
+    Figure
+        { content = content
+        , caption = Nothing
+        }
+
+
+
+-- VIEW
+
+
+view : Figure msg -> Ui.Element msg
+view (Figure config) =
+    Ui.el
+        [ Ui.padding Style.spacing.size2
+        , UiBackground.color (Color.toElmUi Style.color.highlightLight)
+        , Ui.centerX
+        ]
+        config.content
