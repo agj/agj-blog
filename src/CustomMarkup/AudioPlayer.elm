870000e9873e5f356fe2fee39259213b84b2e431
agj <ale@agj.cl>
2023-03-07 01:12:54 -0300

    Further design of AudioPlayer

--- src/CustomMarkup/AudioPlayer.elm
+++ src/CustomMarkup/AudioPlayer.elm
@@ -9,6 +9,7 @@ import CustomMarkup.AudioPlayer.Track exposing (Track)
 import Element as Ui
 import Element.Background as UiBackground
 import Element.Border as UiBorder
+import Element.Input as UiInput
 import Icon
 import Markdown.Html
 import Style
@@ -19,20 +20,20 @@ type alias AudioPlayer =
     }
 
 
+type alias Config msg =
+    { playingTrack : Maybe Track
+    , onStopTrack : Maybe msg
+    , onPlayPauseTrack : Maybe msg
+    }
+
+
 renderer : Markdown.Html.Renderer AudioPlayer
 renderer =
     Markdown.Html.tag "audio-player" AudioPlayer
         |> Markdown.Html.withAttribute "title"
 
 
-toElmUi :
-    { playingTrack : Maybe Track
-    , onStopTrack : Maybe msg
-    , onPlayPauseTrack : Maybe msg
-    }
-    -> AudioPlayer
-    -> List Track
-    -> Ui.Element msg
+toElmUi : Config msg -> AudioPlayer -> List Track -> Ui.Element msg
 toElmUi config audioPlayer tracks =
     Ui.column
         [ UiBackground.color (Style.color.layout05 |> Color.toElmUi)
@@ -40,7 +41,7 @@ toElmUi config audioPlayer tracks =
         [ Ui.text audioPlayer.title
         , Ui.column []
             (tracks
-                |> List.map trackToElmUi
+                |> List.map (trackToElmUi config)
             )
         ]
 
@@ -49,9 +50,17 @@ toElmUi config audioPlayer tracks =
 -- INTERNAL
 
 
-trackToElmUi : Track -> Ui.Element msg
-trackToElmUi track =
-    Ui.row []
-        [ Icon.play Icon.Medium
-        , Ui.text track.title
+trackToElmUi : Config msg -> Track -> Ui.Element msg
+trackToElmUi config track =
+    UiInput.button
+        [ UiBorder.rounded 0
+        , UiBackground.color (Style.color.transparent |> Color.toElmUi)
+        , Ui.width Ui.fill
         ]
+        { onPress = Nothing
+        , label =
+            Ui.row []
+                [ Icon.none Icon.Medium
+                , Ui.text track.title
+                ]
+        }
