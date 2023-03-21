85bcf074258991b1cddc5be0f8700b6d2aa06f05
agj <ale@agj.cl>
2023-03-20 23:48:45 -0300

    Split audio-player custom element into separate file

--- public/index.js
+++ public/index.js
@@ -1,67 +1,6 @@
+import { defineAudioPlayerCustomElement } from './js/custom-elements/audio-player.js';
 
-customElements.define('audio-player', class AudioPlayerElement extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  static get observedAttributes() {
-    return ['src', 'playing', 'current-time'];
-  }
-
-  connectedCallback() {
-    this.audioElement = new Audio();
-
-    this.onTimeUpdate = (event) => {
-      this.dispatchEvent(new CustomEvent('timeupdate', {
-        detail: {
-          currentTime: this.audioElement.currentTime,
-          duration: this.audioElement.duration
-        },
-      }));
-    };
-    this.audioElement.addEventListener('timeupdate', this.onTimeUpdate);
-
-    this.appendChild(this.audioElement);
-    this.update();
-  }
-
-  attributeChangedCallback() {
-    this.update();
-  }
-
-  disconnectedCallback() {
-    this.audioElement?.stop?.();
-    this.audioElement?.removeEventListener('timeupdate', this.onTimeUpdate);
-    this.audioElement = null;
-  }
-
-  update() {
-    if (!this.audioElement) {
-      return;
-    }
-
-    const src = this.getAttribute('src');
-    const playing = this.getAttribute('playing');
-    const currentTime = this.getAttribute('current-time');
-
-    const currentSrc = this.audioElement.getAttribute('src');
-    const currentCurrentTime = this.audioElement.currentTime;
-
-    if (src !== currentSrc) {
-      this.audioElement.setAttribute('src', src);
-    }
-
-    if (Math.abs(currentTime - currentCurrentTime) > 2) {
-      this.audioElement.currentTime = currentTime;
-    }
-
-    if (playing === 'true') {
-      this.audioElement.play?.();
-    } else {
-      this.audioElement.pause?.();
-    }
-  }
-});
+defineAudioPlayerCustomElement();
 
 /** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
 
