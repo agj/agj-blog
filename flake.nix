549eda8029b971d047e127ed2c1a511e94ae6a35
agj <ale@agj.cl>
2023-05-28 01:21:49 -0400

    Change to use Nix flakes instead of niv

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {system = system;};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            elmPackages.elm
+            elmPackages.elm-format
+            elmPackages.elm-optimize-level-2
+            elmPackages.elm-pages
+            elmPackages.elm-review
+          ];
+        };
+      }
+    );
+}
