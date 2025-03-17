62023c3fb8e82b3d66fe2b75727bc61406046932
agj <ale@agj.cl>
2025-03-16 23:33:59 -0300

    Refactor customProperties

--- src/Custom/Html/Attributes.elm
+++ src/Custom/Html/Attributes.elm
@@ -1,11 +1,16 @@
 module Custom.Html.Attributes exposing (customProperties)
 
 import Html exposing (Attribute)
-import Html.Attributes exposing (attribute)
+import Html.Attributes
 
 
+{-| Define a list of CSS custom properties (otherwise known as variables, those
+starting with `--`) and their values as an attribute to set to an element. It
+and its children will have these properties set as defined.
+-}
 customProperties : List ( String, String ) -> Attribute msg
-customProperties =
-    List.map (\( name, value ) -> "--" ++ name ++ ": " ++ value)
-        >> String.join "; "
-        >> attribute "style"
+customProperties propertyNameValuePairs =
+    propertyNameValuePairs
+        |> List.map (\( name, value ) -> "--" ++ name ++ ": " ++ value)
+        |> String.join "; "
+        |> Html.Attributes.attribute "style"
