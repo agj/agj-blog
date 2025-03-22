fe960dba4960bad021fc0c7b217fe49ef21a31a4
agj <ale@agj.cl>
2025-03-22 18:50:09 -0300

    Move color variables to SCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import { elmReviewTailwindCssPlugin } from "elm-review-tailwindcss-postcss-plugin";
+import sass from "@csstools/postcss-sass";
 
 export default {
-  plugins: [tailwindcss(), autoprefixer(), elmReviewTailwindCssPlugin()],
+  syntax: "postcss-scss",
+  plugins: [
+    sass(),
+    tailwindcss(),
+    autoprefixer(),
+    elmReviewTailwindCssPlugin(),
+  ],
 };
