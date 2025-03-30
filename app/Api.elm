65efe7d3a9a90e854931966db9596c635b9ec02f
agj <ale@agj.cl>
2025-03-29 23:25:29 -0300

    Start configuring RSS feeds

--- app/Api.elm
+++ app/Api.elm
@@ -3,12 +3,16 @@ module Api exposing (routes)
 import ApiRoute exposing (ApiRoute)
 import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
+import Head
 import Html exposing (Html)
 import LanguageTag exposing (emptySubtags)
 import LanguageTag.Language
+import Pages
 import Pages.Manifest as Manifest
 import Route exposing (Route)
+import Rss
 import Site
+import Time
 
 
 routes :
@@ -16,11 +20,65 @@ routes :
     -> (Maybe { indent : Int, newLines : Bool } -> Html Never -> String)
     -> List (ApiRoute ApiRoute.Response)
 routes getStaticRoutes htmlToString =
-    [ Manifest.generator Site.canonicalUrl (BackendTask.succeed manifest)
+    [ manifest
+
+    -- Global RSS feed.
+    , ApiRoute.succeed
+        (rss
+            { title = Site.name
+            , description = Site.description
+            , url = Site.canonicalUrl
+            }
+            []
+            |> BackendTask.succeed
+        )
+        |> ApiRoute.literal "rss.xml"
+        |> ApiRoute.single
+        |> ApiRoute.withGlobalHeadTags
+            (BackendTask.succeed [ Head.rssLink "/rss.xml" ])
+
+    -- Tag RSS feeds.
+    , ApiRoute.succeed
+        (\tagName ->
+            rss
+                { title = Site.name
+                , description = Site.description
+                , url = Site.canonicalUrl
+                }
+                []
+                |> BackendTask.succeed
+        )
+        |> ApiRoute.literal "tag"
+        |> ApiRoute.slash
+        -- Tag name.
+        |> ApiRoute.capture
+        |> ApiRoute.slash
+        |> ApiRoute.literal "rss.xml"
+        |> ApiRoute.preRender
+            (\route ->
+                BackendTask.succeed
+                    [ route "javascript" ]
+            )
+        |> ApiRoute.withGlobalHeadTags
+            (BackendTask.succeed [ Head.rssLink "/rss.xml" ])
     ]
 
 
-manifest : Manifest.Config
+{-| `manifest.json` file, describing this website to use as a “progressive web app”.
+
+See: <https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Manifest>
+
+    { title = String
+    , description = String
+    , url = String
+    , lastBuildTime = Time.Posix
+    , generator = Maybe String
+    , items = List Item
+    , siteUrl = String
+    }
+
+-}
+manifest : ApiRoute ApiRoute.Response
 manifest =
     Manifest.init
         { name = Site.name
@@ -32,3 +90,24 @@ manifest =
             (LanguageTag.Language.en
                 |> LanguageTag.build emptySubtags
             )
+        |> BackendTask.succeed
+        |> Manifest.generator Site.canonicalUrl
+
+
+rss :
+    { title : String
+    , url : String
+    , description : String
+    }
+    -> List Rss.Item
+    -> String
+rss config items =
+    Rss.generate
+        { title = Site.name
+        , description = Site.description
+        , url = Site.canonicalUrl
+        , lastBuildTime = Pages.builtAt
+        , generator = Nothing
+        , items = items
+        , siteUrl = Site.canonicalUrl
+        }
