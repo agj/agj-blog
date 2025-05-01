4bb234ebbced3d8474f1feaafd73c3dc192e335c
agj <ale@agj.cl>
2025-05-01 12:52:42 -0400

    Position dropdown horizontally centered with its parent

--- src-ts/custom-elements/dropdown.ts
+++ src-ts/custom-elements/dropdown.ts
@@ -1,3 +1,7 @@
+/**
+ * Custom element `<custom-dropdown>` that uses absolute positioning to place
+ * itself right below its parent element. Useful as a `popovertarget`.
+ */
 export const defineDropdownCustomElement = () => {
   customElements.define(
     "custom-dropdown",
@@ -6,8 +10,18 @@ export const defineDropdownCustomElement = () => {
         super();
       }
 
+      observer?: IntersectionObserver;
+
       connectedCallback() {
         this.reposition();
+
+        this.observer = new IntersectionObserver(() => this.reposition());
+        this.observer.observe(this);
+      }
+
+      disconnectedCallback() {
+        this.observer?.disconnect();
+        this.observer = undefined;
       }
 
       /**
@@ -15,13 +29,18 @@ export const defineDropdownCustomElement = () => {
        */
       reposition() {
         const parentRect = this.parentElement?.getBoundingClientRect();
+        const isVisible = this.checkVisibility();
 
-        if (!parentRect) {
+        if (!parentRect || !isVisible) {
           return;
         }
 
+        const selfRect = this.getBoundingClientRect();
+        const parentWidth = parentRect.right - parentRect.left;
+        const xParentMiddle = parentRect.left + parentWidth / 2;
+        const selfWidth = selfRect.right - selfRect.left;
         const top = parentRect.bottom;
-        const left = parentRect.left;
+        const left = xParentMiddle - selfWidth / 2;
 
         this.style = `inset: unset; top: calc(0.5rem + ${top}px); left: ${left}px`;
       }
